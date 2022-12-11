5f4872406743effa4263f6480317cf023d4619c3
mamfarine@gmail.com <berdizbah.com>
2022-12-11 11:33:35 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main - Entry point
- * return;1
- * */
+
+/**
+ * main -It prints"and that piece of art is useful"
+ * Return:1
+ *
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
